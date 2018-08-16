0ed23b161bbd07aa8f6ec38826a938a21c4396eb
Alexey Solovyev <solovyev.alexey@gmail.com>
2018-08-16 11:36:34 -0700

    Experiments with the tame graph generator

--- hol_core.ml
+++ hol_core.ml
@@ -1,4 +1,11 @@
 include Parser;;
 include Define;;
 
+(* let MY_TEST () =
+  for i = 0 to 10000 do
+    MESON[] `!P Q R. (if P then Q else R) <=> (P ==> Q) /\ (~P ==> R)`
+  done;;
+  
+let () = MY_TEST();; *)
+
 print_endline "hol_core.ml loaded";;
